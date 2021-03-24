2531585cc621f26324ce38c5fb96ea79a81c1856
emirxmertoglu <emirxmertoglu@outlook.com>
2021-03-25 00:31:51 +0300

    delete unnecessary lines

--- slack-clone/assets/js/index.js
+++ slack-clone/assets/js/index.js
@@ -1,10 +1,8 @@
-jQuery(function($) {
-    $(window).scroll(function fix_element() {
-      $('.header-container').css(
-        $(window).scrollTop() > 100
-          ? { 'position': 'fixed', 'top': '10px', "left":'0', 'right': '0', 'background': '#fff'}
-          : { 'position': 'relative', 'top': 'auto', 'background' : 'transparent', 'border-radius' : '50px' }
-      );
-      return fix_element;
-    }());
-  });
+$(window).scroll(function nav_position() {
+  $('.header-container').css(
+    $(window).scrollTop() > 100
+      ? { 'position': 'fixed', 'top': '10px', "left": '0', 'right': '0', 'background': '#fff' }
+      : { 'position': 'relative', 'top': 'auto', 'background': 'transparent', 'border-radius': '50px' }
+  );
+  return nav_position;
+}());
