0bb2a89e4b19c84eb569e878f132eb74fd51b183
emirxmertoglu <emirxmertoglu@outlook.com>
2021-03-26 00:49:13 +0300

    added box-shadow to header

--- slack-clone/assets/js/index.js
+++ slack-clone/assets/js/index.js
@@ -1,8 +1,8 @@
 $(window).scroll(function nav_position() {
   $('.header-container').css(
     $(window).scrollTop() > 100
-      ? { 'position': 'fixed', 'top': '10px', "left": '0', 'right': '0', 'background': '#fff' }
-      : { 'position': 'relative', 'top': 'auto', 'background': 'transparent', 'border-radius': '50px' }
+      ? { 'position': 'fixed', 'top': '10px', "left": '0', 'right': '0', 'background': '#fff', 'box-shadow': '0 4px 40px rgba(0,0,0,.08)' }
+      : { 'position': 'relative', 'top': 'auto', 'background': 'transparent', 'border-radius': '50px', 'box-shadow': 'none' }
   );
   return nav_position;
 });
