c51ad2007768eca60222c51103464ff8910121e0
İbrahim Gedik <ibrahimgediktc@gmail.com>
2021-10-02 23:12:02 +0300

    fix footer nav dropdown

--- slack-clone/assets/js/index.js
+++ slack-clone/assets/js/index.js
@@ -1,15 +1,28 @@
 $(window).scroll(function nav_position() {
-  $('.nav-container').css(
+  $(".nav-container").css(
     $(window).scrollTop() > 100
-      ? { 'position': 'fixed', 'top': '10px', "left": '0', 'right': '0', 'border-radius': '50px', 'background-color': '#fff', 'box-shadow': '0 4px 40px rgba(0,0,0,8%)' }
-      : { 'position': 'relative', 'top': 'auto', 'background': 'transparent', 'box-shadow': 'none' }
+      ? {
+          position: "fixed",
+          top: "10px",
+          left: "0",
+          right: "0",
+          "border-radius": "50px",
+          "background-color": "#fff",
+          "box-shadow": "0 4px 40px rgba(0,0,0,8%)",
+        }
+      : {
+          position: "relative",
+          top: "auto",
+          background: "transparent",
+          "box-shadow": "none",
+        }
   );
   return nav_position;
 });
 
 // Launch Dropdown
 $(".btn-click").on("click", function () {
-  $(".btn-click").toggleClass("btn-filled-dark")
+  $(".btn-click").toggleClass("btn-filled-dark");
   $(".launch-drop-down").toggle();
 });
 
@@ -18,8 +31,8 @@ $(".nav-menu-btn").click(function () {
   $(".mobile-nav").transition({
     opacity: 1,
     x: 0,
-  })
-  $('body').css('overflow', 'hidden');
+  });
+  $("body").css("overflow", "hidden");
 });
 
 // Mobile Menu Close
@@ -27,27 +40,26 @@ $(".mobile-nav-close").click(function () {
   $(".mobile-nav").transition({
     opacity: 0,
     x: 1000,
-  })
-  $('body').css('overflow', 'unset');
+  });
+  $("body").css("overflow", "unset");
 });
 
 // Mobile Product List Expanded
 $(".list-expanded").click(function () {
-  $(".list-expanded").toggleClass('active');
+  $(".list-expanded").toggleClass("active");
   $(".mobile-nav-product-list").toggle();
 });
 
 // Footer Nav List Expanded
-var footerNavList = $(".footer-nav-list-item");
-var footerNavLink = $('.footer-nav-link');
+    var footerNavList = $(".footer-nav-list-item");
+    var footerNavLink = $(".footer-nav-link");
 
-footerNavList.click(function () {
-  if ($(this).hasClass('active')) {
-    $(this).removeClass('active').find(footerNavLink).slideUp();
-  } else {
-    $(".footer-nav-list-item.active .footer-nav-link").slideUp();
-    $('.footer-nav-list-item.active').removeClass('active');
-    $(this).addClass('active').find(footerNavLink).slideDown();
-  }
-  return false;
-})
+    footerNavList.click(function(){
+      if($(this).hasClass('active')){
+        $(this).removeClass('active').find(footerNavLink).removeClass('show');
+      } else {
+        $('.footer-nav-list-item.active .footer-nav-link').removeClass('show');
+        $(".footer-nav-list-item.active").removeClass("active");
+        $(this).addClass('active').find(footerNavLink).addClass('show');
+      }
+    })
